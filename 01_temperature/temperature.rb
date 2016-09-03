68a3f63279cabd6a58fcd35b38169268ebf08256
Hugo Mendoza <hugo.mendoza.ce@gmail.com>
2016-09-02 17:42:22 -0700

    update hello.rb

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(temp)
+	c = 5.0/9.0 * (temp - 32.0)
+	return c
+end
+
+def ctof(temp)
+	f = 9.0/5.0 * temp + 32.0
+	return f
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
-#write your code here
+def add(x, y)
+	x + y
+end
+
+def subtract(x, y)
+	x - y
+end
+
+def sum(input)
+	input.inject(0) { |sum, n| sum + n }
+end
+
+def multiply(input)
+	input.inject(1) { |product, n| product * n }
+end
+
+def power(x, y)
+	x ** y
+end
+
+def factorial(n)
+	if n == 0
+		return 1
+	else (1..n).inject(:*)
+	end
+end
